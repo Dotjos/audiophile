d5bceab79c55ee8cb52277b3089d3c9ea23fdb20
Jose <oladotjos@gmail.com>
2025-11-01 18:16:30 +0100

    UI:mobile--footer-done

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,63 @@
 "use client";
 
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="text-white pt-[52px] pb-[38px] gap-[48px] bg-black flex flex-col items-center">
+      <Image
+        src="/assets/logo.svg"
+        alt=""
+        height={25}
+        width={143}
+        className=""
+      />
+
+      <ul className="text-[13px] text-center font-bold leading-[25px] tracking-[2px] flex flex-col gap-2">
+        <li>HOME</li>
+        <li>HEADPHONES</li>
+        <li>SPEAKERS</li>
+        <li>EARPHONES</li>
+      </ul>
+
+      <p className="opacity-50 px-3 text-center text-Body font-normal">
+        Audiophile is an all in one stop to fulfill your audio needs. We're a
+        small team of music lovers and sound specialists who are devoted to
+        helping you get the most out of personal audio. Come and visit our demo
+        facility - we’re open 7 days a week.
+      </p>
+
+      <p className="opacity-50 px-3 text-center text-Body font-normal">
+        Copyright 2021. All Rights Reserved
+      </p>
+      <ul className="flex items-center gap-x-[40px]">
+        <li>
+          <Image
+            alt="socials"
+            height={24}
+            width={24}
+            src="/assets/shared/desktop/icon-facebook.svg"
+          />
+        </li>
+        <li>
+          <Image
+            alt="socials"
+            height={24}
+            width={24}
+            src="/assets/shared/desktop/icon-twitter.svg"
+          />
+        </li>
+        <li>
+          <Image
+            alt="socials"
+            height={24}
+            width={24}
+            src="/assets/shared/desktop/icon-instagram.svg"
+          />
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
