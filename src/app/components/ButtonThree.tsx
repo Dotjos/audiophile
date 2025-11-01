dc6bd9cc1229c525aee62211c39f26ad166a6263
Jose <oladotjos@gmail.com>
2025-11-01 18:18:04 +0100

    components--button-mobile-done

--- src/app/components/ButtonThree.tsx
+++ src/app/components/ButtonThree.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+interface ButtonThreeProps {}
+
 const ButtonThree = () => {
-  return <button>ShopU+003E</button>;
+  return (
+    <button className="in inline-flex items-center gap-[13.322px]">
+      <span className="uppercase text-[13px] leading-none  tracking-[1px] opacity-50 font-bold">
+        Shop
+      </span>
+      <span className="t text-primary">&gt;</span>
+    </button>
+  );
 };
 
 export default ButtonThree;
