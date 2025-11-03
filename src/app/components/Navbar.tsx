82d0039686dd04ef6a7a6e9f55781b809e42d894
Jose <oladotjos@gmail.com>
2025-11-03 15:52:03 +0100

    update--structure-component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,29 @@
 "use client";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="absolute top-0 bg-transparent z-10 md:px-[40px] lg:px-[165px] w-full">
+      <nav className="flex px-3 md:px-0 my-[32px] lg:my-9">
+        <Image
+          alt="hamburger-nav"
+          src="/assets/hamburger.svg"
+          width={16}
+          height={15}
+          className="lg:hidden border-white"
+        />
+        <Image
+          alt="audiophile-logo"
+          src="/assets/logo.svg"
+          width={143}
+          height={25}
+          className="ml-auto mr-auto md:ml-[42px] lg:ml-0"
+        />
+        <Image alt="cart-nav" src="/assets/carts.svg" width={23} height={20} />
+      </nav>
+      <div className="w-full border-t border-white/[.104]"></div>
+    </div>
+  );
 };
 
 export default Navbar;
